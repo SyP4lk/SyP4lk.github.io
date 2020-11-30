7ad7d620b4c49bf65a2583e034753ce9ba5abeda
SyP4lk <53273626+SyP4lk@users.noreply.github.com>
2020-12-01 03:36:31 +0700

    karta

--- karta/js/script.js
+++ karta/js/script.js
@@ -8,6 +8,67 @@ function newPos (top,left, id){
         left: newLeft
     })
 }
+
+if(width > 800){
+    anime({
+        targets: '#pulse0',
+        translateX: 60,
+        translateY: 28,
+        easing: 'linear',
+        duration: 2000,
+        loop: true
+      });
+      anime({
+          targets: '#pulse1',
+          translateX: 115,
+          translateY: 5,
+          easing: 'linear',
+          duration: 2000,
+          loop: true
+        });
+        anime({
+          targets: '#pulse2',
+          translateX: 315,
+          translateY: 75,
+          easing: 'linear',
+          duration: 2000,
+          loop: true
+        });
+        anime({
+          targets: '#pulse3',
+          translateX: -12,
+          translateY: 15,
+          easing: 'linear',
+          duration: 2000,
+          loop: true
+        });
+        anime({
+          targets: '#pulse4',
+          translateX: -120,
+          translateY: 32,
+          easing: 'linear',
+          duration: 2000,
+          loop: true
+        });
+        anime({
+          targets: '#pulse5',
+          translateX: -118,
+          translateY: 45,
+          easing: 'linear',
+          duration: 2000,
+          loop: true
+        });
+        anime({
+          targets: '#pulse6',
+          translateX: -98,
+          translateY: 45,
+          easing: 'linear',
+          duration: 2000,
+          loop: true
+        });
+}
+
+
 if((width < 800) && (width > 546)){
         newPos(0.13, 0.2, '#newKub');
         newPos(0.107 , 0.225, '#samara');
