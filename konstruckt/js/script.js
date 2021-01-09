e38a45e25b54a12a5be6fc71c2a12a636cf98134
SyP4lk <53273626+SyP4lk@users.noreply.github.com>
2021-01-09 11:23:28 +0700

    ы
    с

--- konstruckt/js/script.js
+++ konstruckt/js/script.js
@@ -5,91 +5,43 @@
       contentText = document.querySelector('.content__text'),
       posBtnName = document.querySelectorAll('.posBtn-name'),
       posBtnText = document.querySelectorAll('.posBtn-text'),
-      text = document.querySelector('#text'),
-      contentName = document.querySelector('.content__name'),
+      text = document.querySelectorAll('#text'),
+      contentName = document.querySelectorAll('.content__name'),
       selected = document.activeElement,
       rightMenu = document.querySelector('.construct-right'),
       inputSize = document.querySelectorAll('input'),
       plus = document.querySelectorAll('.plus'),
       minus = document.querySelectorAll('.minus'),
       textarea = document.querySelectorAll('textarea'),
-      head = document.querySelector('.contentName'),
-      pozd = document.querySelector('.contentText'),
+      head = document.querySelectorAll('.contentName'),
+      pozd = document.querySelectorAll('.contentText'),
       win_w = $(document).width(),
-      content = document.querySelector('.content'),
+      content = document.querySelectorAll('.content'),
       color = document.querySelectorAll('.js-color');
 
   //изменение фона
-  function posContent(number,leftText,topText,leftName,topName,width,height,contWidth){
-      img[number].addEventListener('click', () => {
-        for(let r =0 ; r < imgBg.length; r++){
-          imgBg[r].style.cssText = "display: none;";
-        }
-        imgBg[number].style.cssText = "display: block;";
-        text.style.cssText = "transform: translate("+leftText+"px, "+topText+"px);";
-        contentName.style.cssText = "transform: translate("+leftName+"px, "+topName+"px);width: "+contWidth+"px;";
-        content.style.cssText = 'width: '+width+'px; height: '+height+'px';
-        color[0].innerHTML = 'C Новым Годом!';
-        color[1].innerHTML = 'Текст Поздравления';
-        if(number == 0){
-          for(let i = 0; i<color.length; i++){
-            color[i].style.cssText+='color: #9A9032;font-family: "Amatic SC";';
-          }
-          content.style.cssText = 'width: '+width+'px; height: '+height+'px;margin: 31px auto;';
-          color[0].style.cssText += 'font-size: 50px';
-          color[1].style.cssText += 'font-size: 15px';
-        }
-        if(number == 1){
-          for(let i = 0; i<color.length; i++){
-            color[i].style.cssText+='color: #fff;font-family: "Amatic SC";';
-          }
-          color[0].style.cssText += 'font-size: 100px';
-          color[1].style.cssText += 'font-size: 30px';
-          if(win_w < 500){
-            color[0].style.cssText += 'font-size: 50px';
-            color[1].style.cssText += 'font-size: 15px';
-          }
-        }
-        if(number == 2){
-          for(let i = 0; i<color.length; i++){
-            color[i].style.cssText+='font-family: "Amatic SC";';
-          }
-          color[0].style.cssText += 'color: red;font-size: 120px;';
-          color[1].style.cssText += 'color: #000;font-size: 30px';
-          if(win_w < 500){
-            color[0].style.cssText += 'font-size: 50px';
-            color[1].style.cssText += 'font-size: 15px';
-          }
-        }
-      })
-  }
+  
+for(let i = 0; i < img.length;i++){
+  img[i].addEventListener('click',()=>{
+    for(let r = 0; r < content.length;r++){
+      content[r].style.display = 'none';
+    }
+    for(let j = 0 ; j < pozd.length; j++){
+      text[j].style.cssText = ''
+      pozd[j].innerHTML = 'Текст Поздравления';
+    }
+    for(let l = 0 ; l < head.length; l++){
+      contentName[l].style.cssText = ''
+      head[l].innerHTML = 'С Новым Годом!';
+    }
+    content[i].style.display = 'flex';
+  })
+}
 
-  if(win_w > 500){
-    //drag'n'drop
-    var $draggable = $('.js-drag').draggabilly({
-      containment: true
-    });
-    imgBg[0].style.display = "block";
-    text.style.cssText = "transform: translate(177.6px, 39px);";
-    contentName.style.cssText = "transform: translate(177.6px, 13.2px);width: 200px;";
-    content.style.cssText = 'width: 400px; height: 562px; margin: 0 auto'
-    color[0].style.cssText += 'font-size: 50px;color: #9A9032;font-family: "Amatic SC";';
-    color[1].style.cssText += 'font-size: 15px;color: #9A9032;font-family: "Amatic SC";';
-    posContent(0,177.6,39,177.6,13.2,400,562,200);
-    posContent(1,538.4,113.2,432.8,75.2,'913px',562,400);
-    posContent(2,346.8,247,171.8,190.2,'913px',562,580);
-  }
-  if(win_w < 500){
-    imgBg[0].style.display = "block";
-    content.style.cssText = 'width: 300px; height: 400px;margin: 32px auto;'
-    text.style.cssText = "transform: translate(160px, 20px);";
-    contentName.style.cssText = "transform: translate(135px, 0px);width: 140px;";
-    color[0].style.cssText += 'font-size: 50px;color: #9A9032;font-family: "Amatic SC";';
-    color[1].style.cssText += 'font-size: 15px;color: #9A9032;font-family: "Amatic SC";';
-    posContent(0,160,20,135,0,300,400,140);
-    posContent(1,200,50,180,20,'100%',300,140);
-    posContent(2,135.8,150,60.8,120.2,'913px',300,250);
-  }
+
+  var $draggable = $('.js-drag').draggabilly({
+    containment: true
+  });
     
     if(inputSize[1].style.display == 'flex'){
       plus.addEventListener('click',()=>{
@@ -104,38 +56,42 @@
 
   
 
-  contentName.addEventListener('touchstart',()=>{
-    document.getElementById('editing-name').style.display = 'flex';
-    contentName.style.cssText += ' border: 1px solid #0062b8;';
-  })
-  contentName.addEventListener('touchcancel',()=>{
-    document.getElementById('editing-name').style.display = 'none';
-    contentName.style.cssText += ' border: none;';
-  })
-  text.addEventListener('touchstart',()=>{
-    document.getElementById('editing-text').style.display = 'flex';
-    text.style.cssText += ' border: 1px solid #0062b8;';
-  })
-  text.addEventListener('touchcancel',()=>{
-    document.getElementById('editing-text').style.display = 'none';
-    text.style.cssText += ' border: none;';
-  })
+   for(let i = 0 ; i < contentName.length; i++){
+    contentName[i].addEventListener('touchstart',()=>{
+      document.querySelectorAll('#editing-name')[i].style.display = 'flex';
+      contentName[i].style.cssText += ' border: 1px solid #0062b8;';
+    })
+    contentName[i].addEventListener('touchcancel',()=>{
+      document.querySelectorAll('#editing-name')[i].style.display = 'none';
+      contentName[i].style.cssText += ' border: none;';
+    })
+    text[i].addEventListener('touchstart',()=>{
+      document.querySelectorAll('#editing-text')[i].style.display = 'flex';
+    text[i].style.cssText += ' border: 1px solid #0062b8;';
+    })
+    text[i].addEventListener('touchcancel',()=>{
+      document.querySelectorAll('#editing-text')[i].style.display = 'none';
+      text[i].style.cssText += ' border: none;';
+    })
+   }
 
-  contentName.addEventListener('mouseover',()=>{
-    document.getElementById('editing-name').style.display = 'flex';
-  })
-  contentName.addEventListener('mouseleave',()=>{
-    document.getElementById('editing-name').style.display = 'none';
-  })
-  text.addEventListener('mouseover',()=>{
-    document.getElementById('editing-text').style.display = 'flex';
-  })
-  text.addEventListener('mouseleave',()=>{
-    document.getElementById('editing-text').style.display = 'none';
-  })
+  for(let i = 0; i < contentName.length;i++){
+    contentName[i].addEventListener('mouseover',()=>{
+      document.querySelectorAll('#editing-name')[i].style.display = 'flex';
+    })
+    contentName[i].addEventListener('mouseleave',()=>{
+      document.querySelectorAll('#editing-name')[i].style.display = 'none';
+    })
+    text[i].addEventListener('mouseover',()=>{
+      document.querySelectorAll('#editing-text')[i].style.display = 'flex';
+    })
+    text[i].addEventListener('mouseleave',()=>{
+      document.querySelectorAll('#editing-text')[i].style.display = 'none';
+    })
+  }
 
-
-  document.getElementById('editing-name').addEventListener('click',()=>{
+for(let i = 0;i < document.querySelectorAll('#editing-name').length;i++){
+  document.querySelectorAll('#editing-name')[i].addEventListener('click',()=>{
 
     document.querySelector('.popup').style.display ='block';
     document.querySelector('.black').style.display ='block';
@@ -155,37 +111,36 @@
       inputSize[1].style.display = 'none';
       plus[0].addEventListener('click',()=>{
         inputSize[0].value = parseInt(inputSize[0].value) + 1;
-        if (parseInt(inputSize[0].value) >= 40){
-          inputSize[0].value = 40
-        }
-        contentName.style.cssText += 'font-size: ' + inputSize[0].value + 'px';
+        contentName[i].style.cssText += 'font-size: ' + inputSize[0].value + 'px';
       })
       minus[0].addEventListener('click',()=>{
         inputSize[0].value =  parseInt(inputSize[0].value) - 1;
         if (parseInt(inputSize[0].value) <= 1){
           inputSize[0].value = 1
         }
-        contentName.style.cssText += 'font-size: ' + inputSize[0].value + 'px';
+        contentName[i].style.cssText += 'font-size: ' + inputSize[0].value + 'px';
       })
 
     //позиционирование
-    for(let i = 0 ; i < posBtnName.length; i++){
-      posBtnName[i].style.display = 'flex';
-      posBtnText[i].style.display = 'none';
-      posBtnName[i].addEventListener('click', () => {
-        if(i == 0){
-          contentName.style.cssText += 'text-align: left;'
+    for(let r = 0 ; r < posBtnName.length; r++){
+      posBtnName[r].style.display = 'flex';
+      posBtnText[r].style.display = 'none';
+      posBtnName[r].addEventListener('click', () => {
+        if(r == 0){
+          contentName[i].style.cssText += 'text-align: left;'
         }
-        if(i == 1){
-          contentName.style.cssText += 'text-align: center;'
+        if(r == 1){
+          contentName[i].style.cssText += 'text-align: center;'
         }
-        if(i == 2){
-          contentName.style.cssText += 'text-align: right;'
+        if(r == 2){
+          contentName[i].style.cssText += 'text-align: right;'
         }
     })}
   })
-
-  document.getElementById('editing-text').addEventListener('click',()=>{
+}
+  
+for(let i = 0;i < document.querySelectorAll('#editing-text').length;i++){
+  document.querySelectorAll('#editing-text')[i].addEventListener('click',()=>{
     document.querySelector('.black').style.display ='block';
     document.querySelector('.popup').style.display ='block';
 
@@ -203,17 +158,14 @@
       inputSize[1].style.display = 'flex';
       plus[1].addEventListener('click',()=>{
         inputSize[1].value = parseInt(inputSize[1].value) + 1;
-        if (parseInt(inputSize[1].value) >= 40){
-          inputSize[1].value = 40
-        }
-        text.style.cssText += 'font-size: ' + inputSize[1].value + 'px';
+        text[i].style.cssText += 'font-size: ' + inputSize[1].value + 'px';
       })
       minus[1].addEventListener('click',()=>{
         inputSize[1].value =  parseInt(inputSize[1].value) - 1;
         if (parseInt(inputSize[1].value) <= 1){
           inputSize[1].value = 1
         }
-        text.style.cssText += 'font-size: ' + inputSize[1].value + 'px';
+        text[i].style.cssText += 'font-size: ' + inputSize[1].value + 'px';
       })
     for(let i = 0 ; i < posBtnText.length; i++){
       posBtnName[i].style.display = 'none';
@@ -223,13 +175,15 @@
           text.style.cssText += 'text-align: left;'
         }
         if(i == 1){
-          text.style.cssText += 'text-align: center;'
+          text[i].style.cssText += 'text-align: center;'
         }
         if(i == 2){
-          text.style.cssText += 'text-align: right;'
+          text[i].style.cssText += 'text-align: right;'
         }
       })}
   })
+}
+  
   
   document.querySelector('.black').addEventListener('click', ()=>{
     document.querySelector('.black').style.display ='none';
@@ -246,20 +200,24 @@
 
   
 document.querySelectorAll('.redac')[0].addEventListener('click', ()=>{
-  if(textarea[0].value != ''){
-    head.innerHTML = textarea[0].value;
-  }else{
-    head.innerHTML = 'Заголовок';
+  for(let i=0; i < head.length;i++){
+    if(textarea[0].value != ''){
+      head[i].innerHTML = textarea[0].value;
+    }else{
+      head[i].innerHTML = 'Заголовок';
+    }
   }
   document.querySelector('.popup').style.display ='none';
-    document.querySelector('.black').style.display ='none';
+  document.querySelector('.black').style.display ='none';
 })
 
 document.querySelectorAll('.redac')[1].addEventListener('click', ()=>{
-  if(textarea[1].value != ''){
-    pozd.innerHTML = textarea[1].value;
-  }else{
-    pozd.innerHTML = 'Заголовок';
+  for(let i=0; i < pozd.length;i++){
+    if(textarea[1].value != ''){
+      pozd[i].innerHTML = textarea[1].value;
+    }else{
+      pozd[i].innerHTML = 'Заголовок';
+    }
   }
   document.querySelector('.popup').style.display ='none';
   document.querySelector('.black').style.display ='none';
@@ -267,12 +225,76 @@ document.querySelectorAll('.redac')[1].addEventListener('click', ()=>{
 
 function   saveCanvas(){
   window.scrollTo(0,0);
-  document.getElementById('editing-name').style.display = 'none';
-  contentName.style.cssText += ' border: none;';
-  document.getElementById('editing-text').style.display = 'none';
-  text.style.cssText += ' border: none;';
-  html2canvas(document.querySelector(".content")).then(canvas => {
-    canvas.toBlob(function(blob) {
-        saveAs(blob, "pretty image.jpeg", 0.9);
-    });
-})}
+  for(let i = 0; i < contentName.length;i++){
+    document.querySelectorAll('#editing-name')[i].style.display = 'none';
+    contentName[i].style.cssText += ' border: none;';
+    document.querySelectorAll('#editing-text')[i].style.display = 'none';
+    text[i].style.cssText += ' border: none;';
+    html2canvas(document.querySelectorAll(".content")[i]).then(canvas => {
+      canvas.toBlob(function(blob) {
+        console.log(blob);
+          saveAs(blob, "pretty image.jpeg", 0.9);
+          
+      });
+  })
+  }
+  }
+  let image;
+  document.querySelector('#vk').addEventListener('click',function() {
+  window.scrollTo(0,0);
+  for(let i = 0; i < contentName.length;i++){
+    document.querySelectorAll('#editing-name')[i].style.display = 'none';
+    contentName[i].style.cssText += ' border: none;';
+    document.querySelectorAll('#editing-text')[i].style.display = 'none';
+    text[i].style.cssText += ' border: none;';
+    html2canvas(document.querySelectorAll(".content")[i]).then(canvas => {
+       image = canvas.toDataURL("image/jpeg").replace("image/jpeg","image/octet-stream");
+      console.log(image);
+  })
+  }
+})
+
+Share = {
+	vkontakte: function(purl, ptitle, pimg, text) {
+		url  = 'http://vkontakte.ru/share.php?';
+		url += 'url='          + encodeURIComponent(purl);
+		url += '&title='       + encodeURIComponent(ptitle);
+		url += '&description=' + encodeURIComponent(text);
+		url += '&image='       + encodeURIComponent(pimg);
+		url += '&noparse=true';
+		Share.popup(url);
+	},
+	odnoklassniki: function(purl, text) {
+		url  = 'http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1';
+		url += '&st.comments=' + encodeURIComponent(text);
+		url += '&st._surl='    + encodeURIComponent(purl);
+		Share.popup(url);
+	},
+	facebook: function(purl, ptitle, pimg, text) {
+		url  = 'http://www.facebook.com/sharer.php?s=100';
+		url += '&p[title]='     + encodeURIComponent(ptitle);
+		url += '&p[summary]='   + encodeURIComponent(text);
+		url += '&p[url]='       + encodeURIComponent(purl);
+		url += '&p[images][0]=' + encodeURIComponent(pimg);
+		Share.popup(url);
+	},
+	twitter: function(purl, ptitle) {
+		url  = 'http://twitter.com/share?';
+		url += 'text='      + encodeURIComponent(ptitle);
+		url += '&url='      + encodeURIComponent(purl);
+		url += '&counturl=' + encodeURIComponent(purl);
+		Share.popup(url);
+	},
+	mailru: function(purl, ptitle, pimg, text) {
+		url  = 'http://connect.mail.ru/share?';
+		url += 'url='          + encodeURIComponent(purl);
+		url += '&title='       + encodeURIComponent(ptitle);
+		url += '&description=' + encodeURIComponent(text);
+		url += '&imageurl='    + encodeURIComponent(pimg);
+		Share.popup(url)
+	},
+
+	popup: function(url) {
+		window.open(url,'','toolbar=0,status=0,width=626,height=436');
+	}
+};
